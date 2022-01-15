3208ccb86610e6ce1395883999990162a035b83e
Christian Hinkle <c4hinkle@gmail.com>
2022-01-15 01:15:13 -0500

    start making player chatting loop test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,11 @@ using System.Net.Sockets;
 Console.WriteLine("Hello, World!");
 
 
+// Our UDP socket
 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 socket.Bind(new IPEndPoint(IPAddress.Any, 0));
 
+// Query the STUN server
 for (int i = 0; i < 100; ++i)
 {
     IPEndPoint endpoint = new IPEndPoint(Dns.GetHostAddresses("stun.l.google.com").First(), 19302);
@@ -18,3 +20,22 @@ for (int i = 0; i < 100; ++i)
 
     Console.WriteLine(result.PublicEndPoint.ToString()); 
 }
+
+// Let user connect to another player (via that player's external address)
+Console.WriteLine("Enter an external address to connect to: ");
+IPEndPoint playerEndpoint = IPEndPoint.Parse(Console.ReadLine());
+
+socket.Connect(playerEndpoint);
+while (socket.Connected)
+{
+    socket.Send(new byte[] { 1, 2, 3, 4 });
+
+    byte[] recieved = new byte[1024];
+    int length = socket.Receive(recieved);
+
+    byte[] message = new byte[length];
+    Array.Copy(recieved, message, length);
+    Console.WriteLine("player: " + message.ToString());
+}
+
+Console.WriteLine("Disconnected");
