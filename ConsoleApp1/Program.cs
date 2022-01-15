b772579cdbee2120dbc41a7826bf5983e9c9aa3a
Christian Hinkle <c4hinkle@gmail.com>
2022-01-15 00:48:37 -0500

    add ConsoleApp1 test which queries on google's STUN server

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using STUN;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+
+for (int i = 0; i < 100; ++i)
+{
+    IPEndPoint endpoint = new IPEndPoint(Dns.GetHostAddresses("stun.l.google.com").First(), 19302);
+    STUNQueryResult result = STUNClient.Query(socket, endpoint, STUN.STUNQueryType.PublicIP);
+
+    Console.WriteLine(result.PublicEndPoint.ToString()); 
+}
